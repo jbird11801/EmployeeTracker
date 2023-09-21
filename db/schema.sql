6128747cf27d5b1e703e70dcee7ecf523bae3b26
jbird11801@gmail.com <jbird11801@gmail.com>
2023-09-21 17:18:32 -0400

    feature/sql

---
+++ db/schema.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS employee;
+
+CREATE DATABASE employee;
+
+USE employee;
+
+CREATE TABLE department(
+
+  id INT NOT NULL AUTO_INCREMENT,
+
+  name VARCHAR(30) NOT NULL ,
+
+  PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role(
+
+  id INT NOT NULL AUTO_INCREMENT,
+
+  title VARCHAR(30) NOT NULL ,
+
+  salary DECIMAL NOT NULL , 
+
+  department_id INT , 
+
+  FOREIGN KEY (department_id)
+
+  REFERENCES department(id)
+
+  ON DELETE SET NULL ,
+
+  PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee(
+
+  id INT NOT NULL AUTO_INCREMENT,
+
+  first_name VARCHAR(30) NOT NULL ,
+
+  last_name VARCHAR(30) NOT NULL , 
+
+  role_id INT ,
+
+  manager_id INT, 
+
+  FOREIGN KEY (role_id)
+
+  REFERENCES role(id)
+
+  ON DELETE SET NULL ,
+
+);
