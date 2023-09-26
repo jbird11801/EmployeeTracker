febb7883a334c852394c3bf6855bc2ef2b863943
jbird11801@gmail.com <jbird11801@gmail.com>
2023-09-25 20:06:44 -0400

    feature/ask

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const mysql = require('mysql2');
 
 require('dotenv').config();
 
+const inquirer = require('inquirer');
+
+const question = require('./lib/question.js');
+
+const questionFunc = function (questionIndex , answer , inpMessage)  { 
+  
+  return new question(questionIndex , answer , inpMessage)
+
+};
+
+var index = 1;
+
+var editArryIndex = 0;
 
 const db = mysql.createConnection(
     {
@@ -20,14 +33,78 @@ const db = mysql.createConnection(
 
   ); 
 
-  db.query('SELECT * FROM department;', function (err, results) {
+  function ask (question) {
 
-    if (err){
+    inquirer.prompt(question).then((data) => {
 
-        console.log("Error : " + err);
+      index++;
 
-    };
+      const ansArry = data.answer.split(" : ");
+      
+      if (ansArry[0].toLowerCase() === "veiw"){
 
-    console.table(results);
+      // make sql.js have it do something like this
 
-  });
+        // db.query(`SELECT * FROM ${ansArry[1]};`, function (err, results) {
+
+        //   if (err){
+      
+        //       console.log("Error : " + err);
+      
+        //   };
+      
+        //   console.table(results);
+
+        //   index = 1;
+
+        //   editArryIndex = 0;
+
+        //   ask ( (questionFunc(index , "" , "")).Prompt());
+      
+        // });
+
+      }
+
+      else {
+
+      if ( index === 2){
+
+      ask ( (questionFunc(index , ansArry , "")).Prompt());
+
+      }
+
+      else {
+
+        // revise this part with sql.js file
+
+        if (editArry.finle === true ) {
+
+          index = 1;
+
+          editArryIndex = 0;
+
+          ask ( (questionFunc(index , "" , "")).Prompt());
+          
+        }
+
+        else
+
+        {
+
+          editArryIndex++;
+
+          //use anser on db here
+
+          ask ( (questionFunc(index , "" , "")).Prompt() );
+
+        }
+
+      }
+
+      }
+  
+    });
+
+  }
+
+  ask ( (questionFunc(index , "" , "")).Prompt());
